5d4093e8877f971eb8dd7d00946e4ddde06568f3
Jon Holmes <holmjona@isu.edu>
2019-01-15 10:47:46 -0700

    Added file reading and buttons with a listbox of fun.

--- LearningWPF/LearningWPF/MainWindow.xaml.cs
+++ LearningWPF/LearningWPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,11 +16,48 @@ using System.Windows.Shapes;
 
 namespace LearningWPF {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Jon Holmes
+    /// CS/INFO 1182 
+    /// 15 Jan 2019
+    ///  What it does or is for.
     /// </summary>
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// This gets a file
+        /// </summary>
+        private void BtnGetFile_Click(object sender, RoutedEventArgs e) {
+            try {
+                MessageBox.Show("Trying file");
+string[] lines       =         File.ReadAllLines("../../data/greeneggs.txt");
+                for (int i = 0; i < lines.Length; i++) {
+                    lbLines.Items.Add(lines[i]);
+                }
+                MessageBox.Show("Got file!");
+            }catch(Exception ex) {
+                MessageBox.Show("Oops: " + ex.Message);
+
+            } finally {
+                //MessageBox.Show("always runs");
+
+            }
+            //addStuff(1,2);
+
+
+        }
+
+        /// <summary>
+        /// I have no idea.
+        /// </summary>
+        /// <param name="x">first number</param>
+        /// <param name="y">second number</param>
+        /// <returns></returns>
+        private int addStuff(int x, double y) {
+            int b = 5;
+            return b + x - (int)y;
         }
     }
 }
