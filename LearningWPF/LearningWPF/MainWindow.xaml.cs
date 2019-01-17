5bdc944362daa1a9772fa61ec29829b942fe5f50
Jon Holmes <holmjona@isu.edu>
2019-01-17 10:50:19 -0700

    Added button event handlers. Discussion of hidden and collapsed, stackpanels and object basics.

--- LearningWPF/LearningWPF/MainWindow.xaml.cs
+++ LearningWPF/LearningWPF/MainWindow.xaml.cs
@@ -22,6 +22,7 @@ namespace LearningWPF {
     ///  What it does or is for.
     /// </summary>
     public partial class MainWindow : Window {
+        private int _ButtonID = 0;
         public MainWindow() {
             InitializeComponent();
         }
@@ -32,12 +33,12 @@ namespace LearningWPF {
         private void BtnGetFile_Click(object sender, RoutedEventArgs e) {
             try {
                 MessageBox.Show("Trying file");
-string[] lines       =         File.ReadAllLines("../../data/greeneggs.txt");
+                string[] lines = File.ReadAllLines("../../data/greeneggs.txt");
                 for (int i = 0; i < lines.Length; i++) {
                     lbLines.Items.Add(lines[i]);
                 }
                 MessageBox.Show("Got file!");
-            }catch(Exception ex) {
+            } catch (Exception ex) {
                 MessageBox.Show("Oops: " + ex.Message);
 
             } finally {
@@ -58,6 +59,65 @@ string[] lines       =         File.ReadAllLines("../../data/greeneggs.txt");
         private int addStuff(int x, double y) {
             int b = 5;
             return b + x - (int)y;
+        }
+
+        private int addStuff(int x) {
+            
+            return x;
+        }
+
+        private void BtnAqua_Click(object sender, RoutedEventArgs e) {
+            double dbl = 3;
+            int iii = (int)'1';
+            //MessageBox.Show(iii.ToString());
+            Button btn = (Button)sender;
+            MessageBox.Show(btn.Name);
+
+
+        }
+
+        private void BtnRed_Click(object sender, RoutedEventArgs e) {
+            object thingIFound = this.FindName("btnaq");
+            Button bbbb = (Button)thingIFound;
+            if (bbbb == null) {
+                MessageBox.Show("oopsy!");
+
+            } else
+                MessageBox.Show(bbbb.Name);
+
+        }
+
+        private void BtnPurple_Click(object sender, RoutedEventArgs e) {
+            Button btn = (Button)sender;
+
+            btn.Visibility = Visibility.Collapsed;
+
+        }
+
+        private void BtnGreen_Click(object sender, RoutedEventArgs e) {
+            Button btn = (Button)sender;
+            if (btn.Visibility == Visibility.Visible)
+                btn.Visibility = Visibility.Hidden;
+            else
+                btn.Visibility = Visibility.Visible;
+
+        }
+
+        private void Btn4552345436_Click(object sender, RoutedEventArgs e) {
+            //object thingIFound = this.FindName("btnPurple");
+            //Button btnP = (Button)thingIFound;
+            //btnP.Visibility = Visibility.Visible;
+
+            Button btn = new Button();
+            stkButtons.Children.Add(btn);
+            btn.Content = "Hello!";
+            btn.Name = "ButtonOfAwesomeness" + _ButtonID;
+            _ButtonID++;
+            btn.Click += BtnAqua_Click;
+        }
+
+        private void BtnIAm_Click(object sender, RoutedEventArgs e) {
+            this.Background = new SolidColorBrush(Colors.RosyBrown);
         }
     }
 }
